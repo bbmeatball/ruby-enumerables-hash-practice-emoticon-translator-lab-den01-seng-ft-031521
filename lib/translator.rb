23035fa92e5d2ea0957ee03b041ac3ad2eba8f76
JoAnn Siciliano <joannsiciliano@gmail.com>
2021-02-21 20:05:31 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,7 +19,7 @@ def get_japanese_emoticon(file_path,emoticon)
     library[key][:english]==emoticon
 end
 emoticon ? library[emoticon][:japanese]:"Sorry, that emoticon was not found"
-
+end
 def get_english_meaning
   # code goes here
 end
