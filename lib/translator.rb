5a671b7ba625a4597ffd000e02d454735ce34aa3
JoAnn Siciliano <joannsiciliano@gmail.com>
2021-02-21 20:11:21 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,10 +22,9 @@ emoticon ? library[emoticon][:japanese]:"Sorry, that emoticon was not found"
 end
 def get_english_meaning(file_path, emoticon)
   library=load_library(file_path)
-  emoticon=library.keys.find do |key|
-    library[key][:japanese]==emoticon
+  emoticon = library.keys.find do |key|
+    library [key][:japanese]==emoticon
+    
   end
   emoticon ? emoticon: "Sorry, that emoticon was not found"
-end
-  # code goes here
 end
